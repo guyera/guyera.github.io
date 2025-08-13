ecbee4c26bea9d9431ab33e698a640171665eb7f
Alexander Guyer <guyera@oregonstate.edu>
2025-08-12 18:15:46 -0700

    Make copyable optional for terminalblock

--- app/lecture-notes/ui/terminalblock.tsx
+++ app/lecture-notes/ui/terminalblock.tsx
@@ -1,7 +1,7 @@
 import RehypeCopyableCodeBlock from '@/app/ui/codeblock/rehypecopyablecodeblock'
 import RehypeCodeBlock from '@/app/ui/codeblock/rehypecodeblock'
 
-export default async function TerminalBlock({ children, fileName, copyable=true }: { children?: any, copyable: boolean, fileName?: string }) {
+export default async function TerminalBlock({ children, fileName, copyable=true }: { children?: any, copyable?: boolean, fileName?: string }) {
   return (
     <div className="mb-7 mx-3 text-base">
       {copyable ? <RehypeCopyableCodeBlock code={children} language="none" fileName={fileName}/> : <RehypeCodeBlock code={children} language="none" fileName={fileName}/>}
