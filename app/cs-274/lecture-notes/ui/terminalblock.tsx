198c90bc91d038b2c0f8bfb4941186b821db0c11
Alexander Guyer <guyera@oregonstate.edu>
2025-10-01 08:37:38 -0700

    Add highlightLines option to terminal block

--- app/cs-274/lecture-notes/ui/terminalblock.tsx
+++ app/cs-274/lecture-notes/ui/terminalblock.tsx
@@ -1,10 +1,10 @@
 import RehypeCopyableCodeBlock from '@/app/ui/codeblock/rehypecopyablecodeblock'
 import RehypeCodeBlock from '@/app/ui/codeblock/rehypecodeblock'
 
-export default async function TerminalBlock({ children, fileName, copyable=true }: { children?: any, copyable?: boolean, fileName?: string }) {
+export default async function TerminalBlock({ children, fileName, highlightLines, copyable=true }: { children?: any, copyable?: boolean, highlightLines?: string, fileName?: string }) {
   return (
     <div className="mb-7 mx-3 text-base">
-      {copyable ? <RehypeCopyableCodeBlock code={children} language="none" fileName={fileName}/> : <RehypeCodeBlock code={children} language="none" fileName={fileName}/>}
+      {copyable ? <RehypeCopyableCodeBlock code={children} language="none" fileName={fileName} highlightLines={highlightLines}/> : <RehypeCodeBlock code={children} language="none" fileName={fileName} highlightLines={highlightLines}/>}
     </div>
   )
 }
