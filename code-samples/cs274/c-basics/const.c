04ab746de2ab16641799045b041d2528005d557f
Alexander Guyer <guyera@oregonstate.edu>
2025-12-15 15:38:50 -0800

    More code samples for C basics

---
+++ code-samples/cs274/c-basics/const.c
@@ -0,0 +1,7 @@
+int main() {
+	float x = 5; // x is a variable of type 'float'
+	const float pi = 3.14; // pi is a constant of type 'const float'
+	
+	x = 10; // This is perfectly fine
+	pi = 10; // This causes a compilation error
+}
---
+++ code-samples/cs274/c-basics/scanf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	// Prompt the user for some values
+	printf("Enter your age, followed by a space, followed by your "
+		"favorite number: ");
+
+	// Create variables in which to store the user's provided
+	// values
+	int age;
+	float favorite_number;
+
+	// The user is expected to provide their age and THEN their
+	// favorite number. The first is an int, and the second is a
+	// float, so the format specifiers for these inputs are %d
+	// followed by %f. Pause the program until the user enters
+	// these two values, then store the supplied values in the
+	// 'age' and 'favorite_number' variables, respectively:
+	scanf("%d %f", &age, &favorite_number);
+	// Notice the ampersands above (&age and &favorite_number)
+	
+	// Just to prove it worked, add the two numbers together and
+	// print the output:
+	printf("The sum is %f\n", age + favorite_number);
+}
