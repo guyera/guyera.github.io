7f9101ea715660f39b544f35794b195360a30a11
Alexander Guyer <guyera@oregonstate.edu>
2026-01-02 19:00:47 -0800

    Patch type of caption in image.tsx

--- app/cs-274/lecture-notes/ui/image.tsx
+++ app/cs-274/lecture-notes/ui/image.tsx
@@ -2,7 +2,7 @@ import NextImage from 'next/image'
 
 import Caption from './caption'
 
-export default async function Image({ src, alt, caption }: { src: any, alt: string, caption?: Caption }) {
+export default async function Image({ src, alt, caption }: { src: any, alt: string, caption?: any }) {
   return (
     <>
       <NextImage src={src} alt={alt} className={`ml-auto mr-auto max-w-[100%] ${caption ? 'mb-1' : 'mb-7'}`}/>
