bc44357410d858a6534ec1c8862714558289e37c
Alexander Guyer <guyera@oregonstate.edu>
2026-01-09 12:50:20 -0800

    Forward width and height Image arguments to NextImage

--- app/cs-274/lecture-notes/ui/image.tsx
+++ app/cs-274/lecture-notes/ui/image.tsx
@@ -2,14 +2,14 @@ import NextImage from 'next/image'
 
 import Caption from './caption'
 
-export default async function Image({ src, alt, caption, srcDarkMode }: { src: any, alt: string, caption?: any, srcDarkMode?: any }) {
+export default async function Image({ src, alt, caption, srcDarkMode, width, height }: { src: any, alt: string, caption?: any, srcDarkMode?: any, width?: any, height?: any }) {
 
   if (srcDarkMode) {
     return (
       <>
         <picture>
           <source srcSet={srcDarkMode.src} alt={alt} className={`ml-auto mr-auto max-w-[100%] ${caption ? 'mb-1' : 'mb-7'}`} media="(prefers-color-scheme: dark)" />
-          <NextImage src={src} alt={alt} className={`ml-auto mr-auto max-w-[100%] ${caption ? 'mb-1' : 'mb-7'}`}/>
+          <NextImage src={src} alt={alt} className={`ml-auto mr-auto max-w-[100%] ${caption ? 'mb-1' : 'mb-7'}`} width={width} height={height}/>
         </picture>
         {caption}
       </>
@@ -17,7 +17,7 @@ export default async function Image({ src, alt, caption, srcDarkMode }: { src: a
   } else {
     return (
       <>
-        <NextImage src={src} alt={alt} className={`ml-auto mr-auto max-w-[100%] ${caption ? 'mb-1' : 'mb-7'}`}/>
+        <NextImage src={src} alt={alt} className={`ml-auto mr-auto max-w-[100%] ${caption ? 'mb-1' : 'mb-7'}`} width={width} height={height}/>
         {caption}
       </>
     )
