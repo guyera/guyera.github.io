75d9b50687e1cda3857c0b474466e3dce49b6a26
Alexander Guyer <guyera@oregonstate.edu>
2026-01-09 12:38:25 -0800

    Support alternative dark mode version of images via srcDarkMode

--- app/cs-274/lecture-notes/ui/image.tsx
+++ app/cs-274/lecture-notes/ui/image.tsx
@@ -2,11 +2,24 @@ import NextImage from 'next/image'
 
 import Caption from './caption'
 
-export default async function Image({ src, alt, caption }: { src: any, alt: string, caption?: any }) {
-  return (
-    <>
-      <NextImage src={src} alt={alt} className={`ml-auto mr-auto max-w-[100%] ${caption ? 'mb-1' : 'mb-7'}`}/>
-      {caption}
-    </>
-  )
+export default async function Image({ src, alt, caption, srcDarkMode }: { src: any, alt: string, caption?: any, srcDarkMode?: any }) {
+
+  if (srcDarkMode) {
+    return (
+      <>
+        <picture>
+          <source srcSet={srcDarkMode.src} alt={alt} className={`ml-auto mr-auto max-w-[100%] ${caption ? 'mb-1' : 'mb-7'}`} media="(prefers-color-scheme: dark)" />
+          <NextImage src={src} alt={alt} className={`ml-auto mr-auto max-w-[100%] ${caption ? 'mb-1' : 'mb-7'}`}/>
+        </picture>
+        {caption}
+      </>
+    )
+  } else {
+    return (
+      <>
+        <NextImage src={src} alt={alt} className={`ml-auto mr-auto max-w-[100%] ${caption ? 'mb-1' : 'mb-7'}`}/>
+        {caption}
+      </>
+    )
+  }
 }
